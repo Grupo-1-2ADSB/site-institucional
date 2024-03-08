1695239b1ef148784ec7ed94ec3445be295f168b
Raissa Uyema <raissa.meggiolaro@sptech.school>
2024-03-08 16:44:07 -0300

    Atualização do index

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,10 +1,13 @@
 function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
+    let menuMobile = document.getElementById("menuMobile");
+    // O marcos esteve aqui
+    console.log(menuMobile);
+
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
         document.querySelector('.icon').src = "./assets/images/three-line-menu-icon-24.png";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+        document.querySelector('.icon').src = "./assets/images/three-line-menu-icon-24.png";
     }
 }
