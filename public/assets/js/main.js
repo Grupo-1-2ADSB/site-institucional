267849da7ba78a066113b6b37be1ab8aeb0c9bbf
Raissa Uyema <raissa.meggiolaro@sptech.school>
2024-03-09 22:42:04 -0300

    Criei a caixa de perguntas

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -11,3 +11,13 @@ function menuShow() {
         document.querySelector('.icon').src = "./assets/images/three-line-menu-icon-24.png";
     }
 }
+
+document.addEventListener('click', function(event) {
+    // Verifica se o clique não foi dentro de um 'details'
+    if (!event.target.closest('details')) {
+      // Fecha todos os elementos 'details' abertos
+      document.querySelectorAll('details[open]').forEach(function(details) {
+        details.removeAttribute('open');
+      });
+    }
+  });
