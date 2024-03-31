f3088ea09f26740a33bc85aebb0c05da81d328a6
Raissa Uyema <raissa.meggiolaro@sptech.school>
2024-03-31 08:19:28 -0300

    style(cadastro/style.css): Ajustei a centralização das inputs.

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -4,14 +4,14 @@ var btnColor = document.querySelector('.btnColor')
 
 document.querySelector('#btnSignin')
   .addEventListener('click', () => {
-    formSignin.style.left = "25px"
-    formSignup.style.left = "620px"
+    formSignin.style.left = "90px"
+    formSignup.style.left = "600px"
     btnColor.style.left = "0px"
 })
 
 document.querySelector('#btnSignup')
   .addEventListener('click', () => {
     formSignin.style.left = "-450px"
-    formSignup.style.left = "25px"
+    formSignup.style.left = "90px"
     btnColor.style.left = "110px"
-})
+}) 
