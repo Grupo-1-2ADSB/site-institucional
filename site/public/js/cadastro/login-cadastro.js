c4b732df466bf6484501e320214f11764527ad30
Raissa Uyema <raissa.meggiolaro@sptech.school>
2024-03-31 22:33:57 -0300

    style(cadastro/cadastroR.html, cadastro/cadastroR.css e Js/login-cadastro.js): Arrumei o código do html, arrumei os comandos do css e arrumei o js.

--- site/public/js/cadastro/login-cadastro.js
+++ site/public/js/cadastro/login-cadastro.js
@@ -1,15 +1,15 @@
-const sign_in_btn = document.querySelector("sign-in-btn");
-const sign_up_btn = document.querySelector("sign-up-btn");
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
-const sign_in_btn2 = document.querySelector("sign-in-btn2");
-const sign_up_btn2 = document.querySelector("sign-up-btn2");
+const sign_in_btn2 = document.querySelector("#sign-in-btn2");
+const sign_up_btn2 = document.querySelector("#sign-up-btn2");
 
 sign_up_btn.addEventListener("click", () => {
     container.classList.add("sign-up-mode");
 })
 
 sign_in_btn.addEventListener("click", () => {
-    container.classList.remove("sign-in-mode");
+    container.classList.remove("sign-up-mode");
 })
 
 sign_up_btn2.addEventListener("click", () => {
@@ -17,5 +17,5 @@ sign_up_btn2.addEventListener("click", () => {
 })
 
 sign_in_btn2.addEventListener("click", () => {
-    container.classList.remove("sign-in-mode2");
+    container.classList.remove("sign-up-mode2");
 })
