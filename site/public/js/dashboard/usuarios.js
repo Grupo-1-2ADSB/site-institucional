924968160bf9965201a2fb089437c18c8c76b418
BeatrizGBrandao <beatriz.brandao@sptech.school>
2024-04-12 20:52:36 -0300

    Style (usuarios.html/.css/.js): Mudança de layout da tela e de cores

--- site/public/js/dashboard/usuarios.js
+++ site/public/js/dashboard/usuarios.js
@@ -1,8 +1,13 @@
-const inputSenha = document.querySelector('.olho1');
-const inputRepetirSenha = document.querySelector('.olho2');
-const forms_btn = document.querySelector('.teste');
-
-
+// Função para exibir senhas
 function exibirSenha(input) {
     input.type = (input.type === "password") ? "text" : "password";
 }
+
+// Função para mudar cor da option quando for selecionada no select
+const corSelecionada = "var(--primary)";
+const selectCargo = document.getElementById("select_cargo");
+
+selectCargo.addEventListener("change", function() {
+    this.style.color = corSelecionada;
+});
+
